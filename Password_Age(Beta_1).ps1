4c4f0d8b08256167ae576a71c3eb8a216f54fefa
drewmilizia <drewmilizia@users.noreply.github.com>
2015-09-13 11:16:48 -0400

    User Password Age
    Password calculations from AD

---
+++ Password_Age(Beta_1).ps1
@@ -0,0 +1,15 @@
+﻿$user = Read-Host "Enter username"
+$getuser = get-ADUser -Identity $user -Properties *
+$pls = get-ADUser $user -properties * | Select-Object -ExpandProperty PasswordLastSet
+$date = Get-Date -Format G
+$begin = [datetime]$pls
+$end = [datetime]$date
+
+$passwordAge = ((New-TimeSpan -End $end -Start $pls).Days).toString()
+$pwdAge = (((New-TimeSpan -End $end -Start $pls).Days).toString())
+$daysLeft = (90-$pwdAge).toString() + " days before $user password expires"
+$getuser | Add-Member -NotePropertyName PasswordAgeInDays -NotePropertyValue $passwordAge -Force
+$getuser | Add-Member -NotePropertyName DaysBeforePasswordExpires -NotePropertyValue $daysLeft -Force
+#$getuser.PasswordAgeInDays
+#$getuser.DaysBeforePasswordExpires
+$UserInformation = $getuser | Select-Object Name,samAccountName,LastLogonDate,LastBadPasswordAttempt,PasswordAgeInDays | Format-List
